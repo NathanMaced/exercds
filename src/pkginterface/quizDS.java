a6bf3e6d3e03465d10fe4d423644d83ed4bbc49b
NathanMaced <nathanmacedo02@gmail.com>
2022-09-20 15:22:59 -0300

    Quiz da aula de DS

---
+++ src/pkginterface/quizDS.java
@@ -0,0 +1,34 @@
+package pkginterface;
+
+import javax.swing.JOptionPane;
+
+public class quizDS {
+int acerto=0;
+
+    String x1 = JOptionPane.showInputDialog(null, "Digite a nota do primeiro");
+       double b1 = Double.parseDouble(x1);
+       if(b1==1){
+        acerto++;
+       }
+       String x2 = JOptionPane.showInputDialog(null, "Digite a nota do primeiro");
+       double b2 = Double.parseDouble(x2);
+       if(b2==1){
+        acerto++;
+       }
+       String x3 = JOptionPane.showInputDialog(null, "Digite a nota do primeiro");
+       double b3 = Double.parseDouble(x3);
+       if(b3==1){
+        acerto++;
+       }
+       String x4 = JOptionPane.showInputDialog(null, "Digite a nota do primeiro");
+       double b4 = Double.parseDouble(x4);
+       if(b4==1){
+        acerto++;
+       }
+       String x5 = JOptionPane.showInputDialog(null, "Digite a nota do primeiro");
+       double b5 = Double.parseDouble(x5);
+       if(b5==1){
+        acerto++;
+       }
+       JOptionPane.showMessageDialog(null,acerto);
+}
